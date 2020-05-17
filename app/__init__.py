1bec15be640da9bd0e18c951b9cfa309f4958dd0
Neil <=>
2020-05-17 18:03:51 +0100

    initiate app

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template
 from app.database import setup_db
 
 app = Flask(__name__)
-app.secret_key = 'skaldhskajl903'
+app.secret_key = 'keyhere'
 app.debug = True
 
 setup_db(app)
